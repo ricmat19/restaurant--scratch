1c2ac233efd1745370876e3ff7c07e3b911d089f
Ricardo Del Cueto <ric19mat@gmail.com>
2022-08-28 02:28:08 -0400

    Setup Reservation Form

--- client/pages/reservations.js
+++ client/pages/reservations.js
@@ -2,9 +2,24 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
 import { Grid } from "@mui/material";
+import { useState } from "react";
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { TextField } from "@mui/material";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 //Reservations functional component
 const Reservations = () => {
+  const [value, setValue] = useState(dayjs("2014-08-18T21:11:54"));
+
+  const handleChange = (Dayjs) => {
+    setValue(newValue);
+  };
+
   //Reservations component
   return (
     <Grid>
@@ -14,11 +29,7 @@ const Reservations = () => {
       <Grid>
         <Grid sx={{ position: "relative" }}>
           <Grid className="page-image-div">
-            <img
-              src={`./images/chocolate.jpg`}
-              alt=""
-              className="page-image"
-            />
+            <img src={`./images/chocolate.jpg`} alt="" className="page-image" />
           </Grid>
         </Grid>
         <Grid
@@ -42,7 +53,72 @@ const Reservations = () => {
             fontSize: "16px",
             padding: "40px 80px",
           }}
-        ></Grid>
+        >
+          <Grid>Set your Reservation</Grid>
+          <Grid
+            sx={{
+              padding: "30px 0",
+              backgroundColor: "white",
+            }}
+          >
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Grid
+                sx={{
+                  display: "grid",
+                  gridTemplateColumns: "auto auto auto",
+                  textAlign: "center",
+                }}
+              >
+                <Grid>
+                  <Select
+                    sx={{ width: "100%" }}
+                    renderValue={(selected) => {
+                      if (selected.length === 0) {
+                        return <em>placeholder</em>;
+                      }
+                    }}
+                  >
+                    <MenuItem>placeholder</MenuItem>
+                    <MenuItem>1</MenuItem>
+                    <MenuItem>2</MenuItem>
+                    <MenuItem>3</MenuItem>
+                    <MenuItem>4</MenuItem>
+                    <MenuItem>5</MenuItem>
+                    <MenuItem>6</MenuItem>
+                    <MenuItem>7</MenuItem>
+                    <MenuItem>8</MenuItem>
+                    <MenuItem>9</MenuItem>
+                    <MenuItem>10</MenuItem>
+                    <MenuItem>11</MenuItem>
+                    <MenuItem>12</MenuItem>
+                    <MenuItem>13</MenuItem>
+                    <MenuItem>14</MenuItem>
+                    <MenuItem>15</MenuItem>
+                  </Select>
+                </Grid>
+                <Grid sx={{ textAlign: "center" }}>
+                  <DesktopDatePicker
+                    sx={{ width: "100%" }}
+                    label="Date desktop"
+                    inputFormat="MM/DD/YYYY"
+                    value={value}
+                    onChange={handleChange}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </Grid>
+                <Grid sx={{ textAlign: "center" }}>
+                  <TimePicker
+                    sx={{ width: "100%" }}
+                    label="Time"
+                    value={value}
+                    onChange={handleChange}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </Grid>
+              </Grid>
+            </LocalizationProvider>
+          </Grid>
+        </Grid>
       </Grid>
       {/* Footer component */}
       <Footer />
